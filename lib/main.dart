a298977f48143eb966f04fb967996b09299448c7
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2023-05-22 21:42:13 +0700

    addming cross-screen navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: theme,
       debugShowCheckedModeBanner: false,
-      home: const MealsScreen(title: 'Some category', meals: dummyMeals),
+      home: const CategoriesScreen(),
     );
   }
 }
